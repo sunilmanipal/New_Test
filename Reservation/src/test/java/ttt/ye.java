3cefcf66d8e3004288782f6c13a285b0d29572c9
sunilna <sunilna@M4L-FAC-SUNIL.mul.manipal.net>
2018-07-02 12:41:08 +0530

    send

---
+++ Reservation/src/test/java/ttt/ye.java
@@ -0,0 +1,15 @@
+package ttt;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class ye {
+	@Test
+	public void sun()
+	{
+		Assert.assertEquals("sunil", "sunil");
+		System.out.println("Welcome to manipal");
+	}
+
+}
